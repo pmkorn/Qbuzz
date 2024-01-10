0041492da4a0181c1736a0ef7494986686dc8560
pmkorn <114108811+pmkorn@users.noreply.github.com>
2024-01-10 17:00:04 +0100

    updates

---
+++ big_screen.php
@@ -0,0 +1,97 @@
+<?php
+
+  session_start();
+
+  ini_set('display_errors', 1);
+  ini_set('display_startup_errors', 1);
+  error_reporting(E_ALL);
+
+  if (!isset($_SESSION['employeeID'])) {
+    header('Location: account/login/'); 
+  } else {
+    include('includes/db.inc.php');
+    $employeeID = $_SESSION['employeeID'];
+    $sqlSelectEmployeeData = "SELECT * FROM employees WHERE employeeID = '$employeeID' LIMIT 1";
+    if ($sqlResultSelectEmployeeData = mysqli_query($conn, $sqlSelectEmployeeData)) {
+      while ($row = mysqli_fetch_array($sqlResultSelectEmployeeData)) {
+        $employeeFirstName = $row['employeeFirstName'];
+        $employeeLastName = $row['employeeLastName'];
+        $employeeRole = $row['employeeRole'];
+      }
+    }   
+  }
+
+  include('includes/headertitle.inc.php');
+
+?>
+
+<!doctype html>
+<html lang="nl">
+  <head>
+    <base href="/">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="shortcut icon" href="images/favicon_qbuzz.ico" type="image/x-icon">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap-icons.css">
+    <link href="https://cdn.datatables.net/v/bs5/dt-1.13.6/datatables.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css?q=<?php echo time(); ?>">
+
+    <title>Qbuzz InfraGD | <?php echo $page; ?></title>
+  </head>
+  <body>
+
+    <?php include('includes/navbar.inc.php'); ?>
+
+    <section class="main-content">
+      <div class="container-fluid py-3">
+        <div class="col-md-12">
+          <table class="table table-hover table-bordered table-striped fixed_header">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Stremming</th>
+                <th>PDF</th>
+              </tr>
+            </thead>
+            <tbody class="overflow-scroll vh-100">
+              <?php
+                $i = 1;
+                while ($i <= 40) {
+                  echo '
+                    <tr>
+                      <td>'.$i.'</td>
+                      <td>Steenwijkerstraatweg</td>
+                      <td><i class="bi bi-file-pdf"></i></td>
+                    </tr>
+                  ';
+                  $i++;
+                }
+              ?>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </section>
+
+  </body>
+
+  <script src="js/bootstrap.bundle.js"></script>
+  <script src="js/jquery-3.7.2.js"></script>
+  <script src="https://cdn.datatables.net/v/bs5/dt-1.13.6/datatables.min.js"></script>
+  <script src="js/functions.js"></script>
+  <script>
+    let table = new DataTable('#overzichtStremmingen',{
+      language: {
+        url: '//cdn.datatables.net/plug-ins/1.13.4/i18n/nl-NL.json',
+      },
+      order: [1, 'asc'],
+      lengthMenu: [
+        [10, 25, 50, -1],
+        [10, 25, 50, 'All'],
+      ],
+    });
+  </script>
+
+</html>
