a8d63e61f5566a68da2e501dc7b12951b8c560bd
pmkorn <114108811+pmkorn@users.noreply.github.com>
2024-02-09 20:40:38 +0100

    update

---
+++ busstop_overview.php
@@ -0,0 +1,141 @@
+<?php
+
+  session_start();
+
+  ini_set('display_errors', 1);
+  ini_set('display_startup_errors', 1);
+  error_reporting(E_ALL);
+
+  include('includes/db.inc.php');
+
+  if (!isset($_SESSION['employeeID'])) {
+    header('Location: account/login/'); 
+  } else {
+    $employeeID = $_SESSION['employeeID'];
+    $sqlSelectEmployeeData = "SELECT * FROM employees WHERE employeeID = '$employeeID' LIMIT 1";
+    if ($sqlResultSelectEmployeeData = mysqli_query($conn, $sqlSelectEmployeeData)) {
+      while ($row = mysqli_fetch_array($sqlResultSelectEmployeeData)) {
+        $employeeFirstName = $row['employeeFirstName'];
+        $employeeLastName = $row['employeeLastName'];
+        $employeeRole = $row['employeeRole'];
+      }
+    }   
+  }
+
+  include('includes/headertitle.inc.php');
+
+  $tableBusstopOutput = '';
+  $sqlBusStops = 'SELECT * FROM busstops';
+  if ($sqlResultBusStops = mysqli_query($conn, $sqlBusStops)) {
+    while ($rowBusStop = mysqli_fetch_array($sqlResultBusStops)) {
+      $tableBusstopOutput .= '<tr>';
+        $tableBusstopOutput .= '<td>'.$rowBusStop['busStopName'].'</td>';
+        $tableBusstopOutput .= '<td>'.$rowBusStop['busStopNumber'].'</td>';
+        $tableBusstopOutput .= '<td><i class="busstopid btn btn-link bi bi-pencil-square" data-bs-toggle="modal" data-bs-target="#newWorkOrder'.$rowBusStop['busStopID'].'" data-id="newWorkOrder'.$rowBusStop['busStopID'].'"></i>&nbsp;&nbsp;&nbsp;<i class="bi bi-info-circle"></i>';
+   
+          $tableBusstopOutput .= '<div class="modal fade" id="newWorkOrder'.$rowBusStop['busStopID'].'" tabindex="-1" aria-labelledby="newWorkOrder'.$rowBusStop['busStopID'].'" aria-hidden="true">';
+            $tableBusstopOutput .= '<div class="modal-dialog modal-fullscreen">';
+              $tableBusstopOutput .= '<div class="modal-content">';
+                $tableBusstopOutput .= '<div class="modal-header">';
+                  $tableBusstopOutput .= '<div>';
+                    $tableBusstopOutput .= '<h1 class="modal-title fs-5 w-100" id="exampleModalLabel">'.$rowBusStop['busStopName'].'</h1>';
+                    $tableBusstopOutput .= '<p class="modal-title fs-6 w-100">'.$rowBusStop['busStopNumber'].'</p>';
+                  $tableBusstopOutput .= '</div>';
+                  $tableBusstopOutput .= '<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>';
+                $tableBusstopOutput .= '</div>';
+                $tableBusstopOutput .= '<div class="modal-body">';
+                  $tableBusstopOutput .= '<form id="inputNewWorkOrderForm'.$rowBusStop['busStopID'].'" action="">';
+                    $tableBusstopOutput .= '<div class="mb-3">';
+                      $tableBusstopOutput .= '<label class="form-label" for="inputNewWorkOrderNotification'.$rowBusStop['busStopID'].'"><strong>Omschrijving mankement</strong></label>';
+                      $tableBusstopOutput .= '<textarea class="form-control" name="inputNewWorkOrderNotification" id="inputNewWorkOrderNotification'.$rowBusStop['busStopID'].'" cols="30" rows="10"></textarea>';
+                    $tableBusstopOutput .= '</div>';
+                    $tableBusstopOutput .= '<input type="hidden" id="busStopIDWorkNumber" value="'.$rowBusStop['busStopID'].'" />';
+                    $tableBusstopOutput .= '<input type="hidden" id="workOrderInsertedBy" value="'.$employeeFirstName." ".$employeeLastName.'">';
+                  $tableBusstopOutput .= '</form>';
+                $tableBusstopOutput .= '</div>';
+                $tableBusstopOutput .= '<div class="modal-footer">';
+                  $tableBusstopOutput .= '<button type="button" class="btn btn-danger" data-bs-dismiss="modal">Annuleren&nbsp;<i class="bi bi-bi-x-circle"></i></button>';
+                  $tableBusstopOutput .= '<button type="button" class="btn btn-success btnSaveWorkOrder" id="btnSaveWorkOrder'.$rowBusStop['busStopID'].'" value="'.$rowBusStop['busStopID'].'">Opslaan&nbsp;<i class="bi bi-check"></i></button>';
+                $tableBusstopOutput .= '</div>';
+              $tableBusstopOutput .= '</div>';
+            $tableBusstopOutput .= '</div>';
+          $tableBusstopOutput .= '</div>';
+
+        $tableBusstopOutput .= '</td>';
+      $tableBusstopOutput .= '</tr>';
+    }
+  }
+
+?>
+
+<!doctype html>
+<html lang="nl">
+  <head>
+    <base href="/">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="shortcut icon" href="images/favicon_qbuzz.ico" type="image/x-icon">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap-icons.css">
+    <link href="https://cdn.datatables.net/v/bs5/dt-1.13.6/datatables.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css?q=<?php echo time(); ?>">
+
+    <title>Qbuzz InfraGD | <?php echo $page; ?></title>
+  </head>
+  <body>
+
+    <?php include('includes/navbar.inc.php'); ?>
+
+    <section class="main-content py-5">
+
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-12">
+            <h1 class="section-title mb-3">Halteoverzicht</h1>
+            <hr class="mb-5">
+          </div>
+        </div>
+      </div>
+
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-4">
+            <table id="busStopTable" class="table table-striped table-hover">
+              <thead>
+                <tr>
+                  <th>Haltenaam:</th>
+                  <th>Haltenummer:</th>
+                  <th>Actie</th>
+                </tr>
+              </thead>
+              <tbody>
+                <?php echo $tableBusstopOutput; ?>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+
+    </section>    
+
+  </body>
+
+  <script src="js/bootstrap.bundle.js"></script>
+  <script src="js/jquery-3.7.2.js"></script>
+  <script src="https://cdn.datatables.net/v/bs5/dt-1.13.6/datatables.min.js"></script>
+  <script src="js/functions.js"></script>
+  <script>
+    let table = new DataTable('#busStopTable',{
+      language: {
+        url: '//cdn.datatables.net/plug-ins/1.13.4/i18n/nl-NL.json',
+      },
+      order: [1, 'asc'],
+      lengthMenu: [
+        [10, 25, 50, -1],
+        [10, 25, 50, 'All'],
+      ],
+    });
+  </script>
+
+</html>
--- includes/navbar.inc.php
+++ includes/navbar.inc.php
@@ -39,7 +39,7 @@
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="" role="button" data-bs-toggle="dropdown" aria-expanded="false">Haltes</a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Overzicht</a></li>
+            <li><a class="dropdown-item" href="haltes/overzicht/">Overzicht</a></li>
             <li><a class="dropdown-item" href="haltes/werkorders/">Werkorders</a></li>
           </ul>
         </li>
