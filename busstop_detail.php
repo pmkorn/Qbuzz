12cc705f08b62e55c9b22ae9f38fdf6c12340402
pmkorn <114108811+pmkorn@users.noreply.github.com>
2024-02-20 14:45:57 +0100

    updates

--- busstop_detail.php
+++ busstop_detail.php
@@ -56,6 +56,42 @@
 
     <?php include('includes/navbar.inc.php'); ?>
 
+    <!-- MODAL -->
+    <div class="modal fade" id="newBusstopWorkorder" aria-hidden="true" aria-labelledby="newBusstopWorkorder" tabindex="-1">
+      <div class="modal-dialog modal-xl modal-dialog-centered">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h1 class="modal-title fs-5" id="newBusstopWorkorder">Werkorder</h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <form id="formAddNewBusstopWorkorder">
+              <div class="form-group mb-3">
+                <label class="form-label" for="inputNewWorkOrderNotification'.$rowBusStop['busStopID'].'"><strong>Categorie</strong></label>                        
+                <select id="inputNewWorkOrderNotification'.$rowBusStop['busStopID'].'" class="form-select">
+                  <option selected>Kies categorie</option>
+                  <option>Haltebord</option>
+                  <option>Haltevertrekstaat</option>
+                  <option>Haltepaal</option>
+                  <option>Abriposter</option>
+                <select>
+              </div>
+              <div class="form-group mb-3">
+                <label class="form-label" for="inputNewWorkOrderNotification'.$rowBusStop['busStopID'].'"><strong>Omschrijving</strong></label>
+                <textarea class="form-control" name="" id="" cols="30" rows="10"></textarea>
+              </div>
+            </form>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Annuleren&nbsp;<i class="bi bi-x-circle"></i></button>
+            <button type="button" class="btn btn-warning text-white" data-bs-dismiss="modal">Reset&nbsp;<i class="bi bi-arrow-counterclockwise"></i></button>
+            <button class="btn btn-success">Opslaan <i class="bi bi-floppy"></i></button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- END MODAL -->
+
     <section class="main-content py-5">
 
       <div class="container-fluid">
@@ -67,54 +103,93 @@
         </div>
       </div>
 
-      <div class="container-fluid py-5">
+      <div class="container-fluid">
         <div class="row">
-          <div class="col-md-3">
-
-            <div class="haltebord">
-              <div class="haltebord-header text-center py-3">
-                <span class="haltebord-zonepart zone"><?php echo substr($busStopNumber, 0, 4) ?></span>
-                <img src="images/bus_logo.jpg" class="rounded" alt="Logo bus" title="Logo bus" height="100px">
-                <span class="haltebord-zonepart"><?php echo substr($busStopNumber, 4, 8) ?></span>
-              </div>
-              <div class="haltebord-body">
-                <div class="haltebord-haltenaam py-2 fs-5"><?php echo $busStopName; ?></div>
-              </div>
-              <div class="haltebord-footer">
-                <div class="haltebord-logo py-2">
-                  <img src="images/qbuzz-logo.png" alt="Logo Qbuzz" title="Logo Qbuzz" height="100px">
-                </div>
-              </div>
-            </div>
-
-          </div>
-          <div class="col-md-9">
+          <div class="col-md-12">
             <div class="container-fluid">
               <div class="row">
                 <div class="col-md-12">
-                  <button class="btn btn-success mb-3">Nieuwe werkorder <i class="bi bi-pencil-square"></i></button>
+                  <button type="button" class="btn btn-success mb-3" data-bs-toggle="modal" data-bs-target="#newBusstopWorkorder">Nieuwe werkorder <i class="bi bi-pencil-square"></i></button>
                 </div>
               </div>
               <div class="row">
-                <div class="col-md-12">
+                <div class="col-md-9">
                   <table class="table table-striped table-hover">
                     <thead>
                       <tr>
-                        <th>#</th>
-                        <th>Omschrijving</th>
+                        <th>Incident nr.</th>
+                        <th>Categorie</th>
+                        <th>Incident</th>
                         <th>Status</th>
                         <th>Actie</th>
                       </tr>
                     </thead>
                     <tbody>
                       <tr>
-                        <td>0000-00-00-00-1</td>
+                        <td>00-00-00-1</td>
+                        <td>Haltevertrekstaat</td>
                         <td>Haltevertrekstaat mist bij de halte.</td>
-                        <td><span class="badge bg-warning text-white">Pending</span></td>
-                        <td></td>
+                        <td><span class="badge bg-warning text-white">In afwachting...</span></td>
+                        <td>
+                          <i class="bi bi-check2 text-success me-3"></i><i class="bi bi-check2-all text-success me-3"></i>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>00-00-00-2</td>
+                        <td>Haltevertrekstaat</td>
+                        <td>Haltevertrekstaat mist bij de halte.</td>
+                        <td><span class="badge bg-success text-white">Gereed <i class="bi bi-check2"></i></span></td>
+                        <td>
+                          <i class="bi bi-check2 text-success me-3"></i><i class="bi bi-check2-all text-success me-3"></i>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>00-00-00-3</td>
+                        <td>Haltevertrekstaat</td>
+                        <td>Haltevertrekstaat mist bij de halte.</td>
+                        <td><span class="badge bg-danger text-white">Lang lopend <i class="bi bi-exclamation-triangle"></i></span></td>
+                        <td>
+                          <i class="bi bi-check2 text-success me-3"></i><i class="bi bi-check2-all text-success me-3"></i>
+                        </td>
                       </tr>
                     </tbody>
                   </table>
+                </div>
+                <div class="col-md-3">
+                  <h1 class="section-title mb-3">Werkorders historie</h1>
+                  <hr class="mb-5">
+                  <div class="accordion accordion-flush" id="accordionFlushExample">
+                    <div class="accordion-item">
+                      <h2 class="accordion-header">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                          Accordion Item #1
+                        </button>
+                      </h2>
+                      <div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                        <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion body.</div>
+                      </div>
+                    </div>
+                    <div class="accordion-item">
+                      <h2 class="accordion-header">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                          Accordion Item #2
+                        </button>
+                      </h2>
+                      <div id="flush-collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                        <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the second item's accordion body. Let's imagine this being filled with some actual content.</div>
+                      </div>
+                    </div>
+                    <div class="accordion-item">
+                      <h2 class="accordion-header">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+                          Accordion Item #3
+                        </button>
+                      </h2>
+                      <div id="flush-collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                        <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
