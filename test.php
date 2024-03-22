ae2a8ed08ca60ced1f25fb86c78f8066a153e7f5
pmkorn <114108811+pmkorn@users.noreply.github.com>
2024-03-22 21:00:50 +0100

    update

--- test.php
+++ test.php
@@ -8,18 +8,28 @@
 
 
   // echo $intval->format("Verschil is: %a");
-  $dateString = date('Ymd'); //Generate a datestring.
-  $branchNumber = 101; //Get the branch number somehow.
-  $receiptNumber = 1;  //You will query the last receipt in your database 
-  //and get the last $receiptNumber for that branch and add 1 to it.;
+  // $dateString = date('Ymd'); //Generate a datestring.
+  // $branchNumber = 101; //Get the branch number somehow.
+  // $receiptNumber = 1;  //You will query the last receipt in your database 
+  // //and get the last $receiptNumber for that branch and add 1 to it.;
   
-  if($receiptNumber < 9999) {
+  // if($receiptNumber < 9999) {
   
-    $receiptNumber = $receiptNumber + 1;
+  //   $receiptNumber = $receiptNumber + 1;
   
-  }else{
-   $receiptNumber = 1;
-  }
-  echo $dateString . '-' . $branchNumber . '-' . $receiptNumber;
+  // }else{
+  //  $receiptNumber = 1;
+  // }
+  // echo $dateString . '-' . $branchNumber . '-' . $receiptNumber;
+
+    $gebruikersnaam = "FrederikeBosma";
+    $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    $length = 8;
+    $wachtwoord = substr(str_shuffle($chars),0,$length);
+    
+    echo "Gebruikersnaam: <strong>" . $gebruikersnaam . "</strong>.<br><br>";
+    echo "Wachtwoord: <strong>FBosma-" . $wachtwoord .  "</strong>.<br><br>";
+    echo "md5 = " . md5($wachtwoord) . "<br>";
+
 
 ?>
