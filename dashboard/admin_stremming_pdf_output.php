a8cf2d492f7a3b6cc0311b0f982987f9bf611087
pmkorn <patrick.matthew.korn@gmail.com>
2024-12-03 17:00:16 +0100

    update

--- dashboard/admin_stremming_pdf_output.php
+++ dashboard/admin_stremming_pdf_output.php
@@ -18,11 +18,20 @@
     require('../fpdf.php');
 
     $pdf = new FPDF();
-    $pdf->AddPage();
-    $pdf->Image('../images/qbuzz-logo.png', 10, 10, 50);
-    $pdf->SetXY(80, 30);
-    $pdf->SetFont('Arial','B',16);
-    $pdf->Cell(50,0,$obstructionType,0,0, 'C');
-    $pdf->Cell(50,0,str_replace(",",", ",$obstructionLines),0,0, 'C');
+    $pdf->AddPage('P', 'A4');
+    $pdf->Image('../images/qbuzz-logo.png', 20, 10, 40);
+    $pdf->SetXY(63.3, 10);
+    $pdf->SetFont('Arial','B',14);
+    // $pdf->Cell(50,0,$obstructionType,0,0, 'C');
+    // $pdf->Cell(50,0,str_replace(",",", ",$obstructionLines),0,0, 'C');
+    // $pdf->Ln(10);
+    // $pdf->Line(10, 40, 200, 40);
+    $pdf->Cell(10);
+    $pdf->Cell(63.3, 10, $obstructionType, 1 , 0, 'C');
+    $pdf->Cell(63.3, 10, '', 1 , 0, 'R');
+    $pdf->Ln(10);
+    $pdf->Cell(63.3, 10, '', 1 , 0, 'L');
+    $pdf->Cell(63.3, 10, $obstructionNumber, 1 , 0, 'C');
+    $pdf->Cell(63.3, 10, '', 1 , 0, 'R');
     $pdf->Output();
 ?>
